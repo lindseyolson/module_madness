ba73ef9a800a4b87859ebc0753ec0e2e2074cda7
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-05-23 17:49:00 -0500

    got currency conversion to work

--- convert-to-dollars.js
+++ convert-to-dollars.js
@@ -1,30 +1,11 @@
 //module 2
 
 function convertToDollar(number) {
-  var dollars = '';
-  dollars += number;
-  dollars.split();
-  // dollars.reverse();
-  // dollars.join();
-  // dollars.replace(/(\d{3}(?!$))/g, "$1,");
-  // dollars.split().reverse().join();
-  // for (var i = dollars.length-1; i >= 0; --i) {
-  //
-  // }
-
-
-
-
-
-  return '$' + dollars + '.00';
-  //create empty string '$'
-  //number.toFixed(2) - cuts off at hundreds place
-  //loop -
-  // while (dolalrs) {
-  //
-  // }
-
-  //$x,xxx.00
+  number = number.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+  return number;
 }
 
 module.exports = convertToDollar;
